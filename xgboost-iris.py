1009e5b0eac42a26a1c5e2a522407f83f908edfe
kuanhc96 <kuanhc96@uchicago.edu>
2024-02-03 16:06:09 +0800

    use xgboost for iris

---
+++ xgboost-iris.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import xgboost as xgb
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+iris = load_iris()
+X = pd.DataFrame(iris.data, columns=iris.feature_names)
+y = pd.Series(iris.target)
+
+print(X)
+print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+xgb_model = xgb.XGBRegressor(
+    n_estimators=100, # number of trees
+    reg_lambda=1, # lambda value for L2 regularization
+    gamma=0, # gamma min reduction of 
+    max_depth=4
+)
+
+# fit the training data in the model
+xgb_model.fit(X_train, y_train)
+
+feature_importances = pd.DataFrame(xgb_model.feature_importances_.reshape(1, -1), columns = iris.feature_names)
+
+predictions = xgb_model.predict(X_test)
+error = mean_squared_error(predictions, y_test)
+print(error)
